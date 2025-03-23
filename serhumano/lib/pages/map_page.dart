932b4cfbb4b3b76dcfd19069e031b62bc14c2497
Ivanovich Chiu <2bavilaoiac@gmail.com>
2025-03-23 14:47:57 -0700

    add locationMarkers

--- serhumano/lib/pages/map_page.dart
+++ serhumano/lib/pages/map_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -8,8 +11,35 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  // Set initial location to the center of the map
+  static const LatLng initialLocation = LatLng(37.7749, -122.4194);
+
+  // Another location in map
+  static const LatLng anotherLocation = LatLng(37.7599, -122.4148);
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    // View of widget Google Maps API
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: initialLocation,
+          zoom: 13,
+        ),
+        zoomControlsEnabled: true, // Enable zoom in/out buttons
+        markers: {
+          Marker(
+            markerId: MarkerId("currentLocation"),
+            icon: BitmapDescriptor.defaultMarker,
+            position: initialLocation,
+          ),
+          Marker(
+            markerId: MarkerId("anotherLocation"),
+            icon: BitmapDescriptor.defaultMarker,
+            position: anotherLocation,
+          ),
+        },
+      ),
+    );
   }
 }
