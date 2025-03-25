a23cb118d2b46ba8e076d6c16d6fb250e5f790c9
Ivanovich Chiu <2bavilaoiac@gmail.com>
2025-03-25 11:06:08 -0700

    add location, routes markers and waypoints

--- serhumano/lib/pages/map_page.dart
+++ serhumano/lib/pages/map_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
-
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:serhumano/consts.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -12,7 +13,7 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   // Location instance
-  Location locationController = new Location();
+  Location locationController = Location();
 
   // Set initial location to the center of the map
   static const LatLng initialLocation = LatLng(37.7749, -122.4194);
@@ -20,19 +21,26 @@ class _MapPageState extends State<MapPage> {
   // Another location in map
   static const LatLng anotherLocation = LatLng(37.7599, -122.4148);
 
-  LatLng? currentL = null;
+  LatLng? currentL;
+
+  GoogleMapController? _mapController; // Controller for GoogleMap
+
+  Map<PolylineId, Polyline> polylines = {};
 
   @override
   void initState() {
     super.initState();
-
-    getLocationUpdates();
+    currentL = initialLocation; // Set a default location
+    getLocationUpdates().then((_) => {
+      getPolyLinePoints().then((coordinates) => {
+        generatePolyLinesFromPoints(coordinates),
+      }),
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // If current position is null, show loading; otherwise, show the map
       body: currentL == null
           ? const Center(
               child: Text("Loading..."),
@@ -42,7 +50,12 @@ class _MapPageState extends State<MapPage> {
                 target: currentL!, // Use the updated currentL value
                 zoom: 13,
               ),
-              zoomControlsEnabled: true, // Enable zoom in/out buttons
+              zoomControlsEnabled: true,
+              myLocationEnabled: true, // Enable the "My Location" layer
+              myLocationButtonEnabled: true, // Enable the "My Location" button
+              onMapCreated: (GoogleMapController controller) {
+                _mapController = controller; // Assign the controller
+              },
               markers: {
                 Marker(
                   markerId: MarkerId("currentLocation"),
@@ -50,16 +63,21 @@ class _MapPageState extends State<MapPage> {
                   position: currentL!, // Use the updated currentL value
                 ),
                 Marker(
-                  markerId: MarkerId("anotherLocation"),
+                  markerId: MarkerId("sourceLocation"),
+                  icon: BitmapDescriptor.defaultMarker,
+                  position: initialLocation,
+                ),
+                Marker(
+                  markerId: MarkerId("destinationLocation"),
                   icon: BitmapDescriptor.defaultMarker,
                   position: anotherLocation,
                 ),
               },
+              polylines: Set<Polyline>.of(polylines.values),
             ),
     );
   }
 
-  // Add a method to get the current location
   Future<void> getLocationUpdates() async {
     try {
       // Check if location services are enabled
@@ -95,6 +113,11 @@ class _MapPageState extends State<MapPage> {
           setState(() {
             currentL = LatLng(currentLocation.latitude!, currentLocation.longitude!);
             print("Current location: $currentL");
+
+            // Move the camera to the new location
+            _mapController?.animateCamera(
+              CameraUpdate.newLatLng(currentL!),
+            );
           });
         }
       });
@@ -105,4 +128,44 @@ class _MapPageState extends State<MapPage> {
       });
     }
   }
+
+  Future<List<LatLng>> getPolyLinePoints() async {
+    List<LatLng> polylineCoordinates = [];
+    PolylinePoints polylinePoints = PolylinePoints();
+
+    // Use the request object in the function call
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      googleApiKey: GOOGLE_MAPS_API_KEY,
+      request: PolylineRequest(
+        origin: PointLatLng(initialLocation.latitude, initialLocation.longitude),
+        destination: PointLatLng(anotherLocation.latitude, anotherLocation.longitude),
+        mode: TravelMode.driving,
+      ),
+    );
+    if (result.points.isNotEmpty) {
+      for (var point in result.points) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      }
+    }
+    else{
+      print("Error in getPolyLinePoints: ${result.errorMessage}");
+    }
+
+    return polylineCoordinates;
+  }
+
+void generatePolyLinesFromPoints(List<LatLng> polylineCoordinates) async{
+  PolylineId id = PolylineId("poly");
+  Polyline polyline = Polyline(
+    polylineId: id, 
+    color: Colors.black,
+    points: polylineCoordinates,
+    width: 8
+    );
+
+    setState(() {
+      polylines[id] = polyline;
+    });
+}
+
 }
